5aab649d9bc573ce90f5be1e7f414d2c774e439f
SonuMinzz <sonuminzz800@gmail.com>
2022-12-13 16:23:03 +0530

    changes made

--- src/main/java/com/sharon/springboot/GitLearning.java
+++ src/main/java/com/sharon/springboot/GitLearning.java
@@ -10,6 +10,6 @@ public class GitLearning {
     @GetMapping("/message")
     public String message()
     {
-        return "this is git learning revision !!";
+        return "This is git learning revision + !!";
     }
 }
